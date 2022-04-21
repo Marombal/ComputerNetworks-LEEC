bf3f0e632d0716aefa162dcb91c86e0a83b1706d
Marombal <101947993+Marombal@users.noreply.github.com>
2022-04-21 14:54:35 +0100

    who's 'SegFault' ? - asked ze

--- geral.c
+++ geral.c
@@ -22,7 +22,7 @@ int setupSerialTerminal(linkLayer connectionParameters){
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VTIME]    = connectionParameters.timeOut*10;   /* inter-character timer unused */
     newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
 
     tcflush(fd, TCIOFLUSH);
